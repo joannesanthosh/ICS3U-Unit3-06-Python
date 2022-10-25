1ed1eb109af8d5c0871a6258fc7400056ba40ec9
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-25 15:24:03 +0000

    init

---
+++ guessing_game_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Oct 2019
+# This program uses a try statement
+
+
+def main():
+    # this function uses a try statement
+
+    # input
+    integer_as_string = input("Enter an integer: ")
+    print("")
+
+    # process & output
+    try:
+        integer_as_number = int(integer_as_string)
+        print("You entered an integer correctly")
+    except ValueError:
+        print("This was not an integer")
+    finally:
+        print("Thanks for playing")
+
+
+if __name__ == "__main__":
+    main()
